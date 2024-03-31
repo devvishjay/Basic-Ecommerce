47be4dfe06461f63076d4e526bff3b5b57e3e861
devvishjay <devvishjay@gmail.com>
2024-03-31 14:23:52 +0530

    Lazy Load

--- src/components/productGrid/productGrid.tsx
+++ src/components/productGrid/productGrid.tsx
@@ -1,17 +1,19 @@
 
-import { FC } from 'react';
-import ProductCard from '../productCard/productCard';
+import { FC, lazy, Suspense } from 'react';
+const ProductCard = lazy(() => import('../productCard/productCard'));
 import styles from './productGrid.module.css';
-import {ProductGridProps} from '../../interfaces/product';
+import { ProductGridProps } from '../../interfaces/product';
 
-const ProductsGrid:FC<ProductGridProps> = ({ products }) => {
+const ProductsGrid: FC<ProductGridProps> = ({ products }) => {
   return (
     <div className={styles.main}>
-    <div className={styles.productsContainer}>
-      {products.map(product => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </div>
+      <div className={styles.productsContainer}>
+        <Suspense fallback={<div>Loading...</div>}>
+          {products.map(product => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </Suspense>
+      </div>
     </div>
   );
 }
